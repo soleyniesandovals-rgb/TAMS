2c11becc130b1882073861923cd92f60bae90e91
Soleyni Sandoval <soleynie.sandovals@gmail.com>
2026-09-23 20:48:45 -0400

    Agrega enums del dominio (TipoCentro, TipoTecnico, TipoEvaluacion, DiaSemana)

---
+++ src/Tams.Negocio/Domain/Enums/TipoCentro.cs
@@ -0,0 +1,8 @@
+namespace Tams.Negocio.Domain.Enums;
+
+/// <summary>Tipo de un centro educativo: normal o técnica.</summary>
+public enum TipoCentro
+{
+    Normal = 0,
+    Tecnica = 1,
+}
---
+++ src/Tams.Negocio/Domain/Enums/TipoEvaluacion.cs
@@ -0,0 +1,8 @@
+namespace Tams.Negocio.Domain.Enums;
+
+/// <summary>Modalidad de evaluación de una calificación: por periodo o por RA.</summary>
+public enum TipoEvaluacion
+{
+    Periodo = 0,
+    RA = 1,
+}
