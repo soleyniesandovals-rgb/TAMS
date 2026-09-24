bd91a21a265489c9ebe6ec17e3619dbcb1cd84cd
Soleyni Sandoval <soleynie.sandovals@gmail.com>
2026-09-23 20:48:45 -0400

    Agrega enums del dominio

---
+++ src/Tams.Negocio/Domain/Enums/TipoTecnico.cs
@@ -0,0 +1,13 @@
+namespace Tams.Negocio.Domain.Enums;
+
+/// <summary>Especialidad técnica ofertada por el centro.</summary>
+public enum TipoTecnico
+{
+    Software = 0,
+    Redes = 1,
+    Electronica = 2,
+    Robotica = 3,
+    Ciberseguridad = 4,
+    Multimedia = 5,
+    DisenoGrafico = 6,
+}
